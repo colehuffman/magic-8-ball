57855bc950e1f8de7752e0f678fc4f77bb017830
colehuffman <79929352+colehuffman@users.noreply.github.com>
2021-03-17 22:52:49 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,27 @@
-let Number2 = 0
-input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-})
+// runs inside code when shaken
 input.onGesture(Gesture.Shake, function () {
+    // Displays "ASK" on the screen to tell user to ask a question
     basic.showString("ASK")
-    basic.pause(5000)
+    // has a two second delay so that the microbit doesn't give an answer before the question is finished being asked
+    basic.pause(2000)
+    // Number variable is set to pick a random number from 0 to 2
     Number2 = randint(0, 2)
+    // if the number picked is 2 a yes will display followed by music
+    // if the number picked is 1 a no will display followed by music
     if (Number2 == 2) {
-        basic.showString("Y")
+        basic.showString("yes")
+        music.playMelody("C G C5 - - - - - ", 500)
     } else if (Number2 == 1) {
-        basic.showString("N")
+        basic.showString("no")
+        music.playMelody("C5 F C - - - - - ", 500)
     } else {
-        basic.showString("M")
+        // if the number picked is 0 a maybe will display followed by music
+        basic.showString("maybe")
+        music.playMelody("F G F G - - - - ", 500)
     }
-    music.playMelody("C5 B E F F D G B ", 120)
+    // displays an 8 to symbolize that this is a magic 8 ball
+    basic.showString("8")
 })
+let Number2 = 0
+// displays an 8 to symbolize that this is a magic 8 ball
+basic.showString("8")
